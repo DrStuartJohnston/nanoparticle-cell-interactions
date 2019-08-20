b549bf7db7ca21f5f49f6f336b2e2f47ea9ebb9d
DrStuartJohnston <47995032+DrStuartJohnston@users.noreply.github.com>
2019-08-20 14:40:24 +1000

    Remove extraneous files

--- Voxel-based model/Particle Properties/Parameters_633nm_Coreshell_Hela.m
+++
@@ -1,7 +0,0 @@
-PDE_Parameters.diameter = 633*1e-9;                            %Scaling parameter for the nanoparticles
-PDE_Parameters.particle_Density = 1850;                         %Density of particles (kg/m^3)
-PDE_Parameters.affinity = 3.06e-11/PDE_Parameters.dx;            %Boundary condition flux condition parameter
-PDE_Parameters.K = 4;                                          %Carrying capacity NP/Cell
-PDE_Parameters.num_Cells = 100000;
-PDE_Parameters.cell_Area = 1600e-12;
-PDE_Parameters.initial_Particle_Count = 1e7;                                %Number of particles at the beginning of the experiment
--- Voxel-based model/Particle Properties/Parameters_Static_PMA_Coreshell.m
+++
@@ -1,7 +0,0 @@
-PDE_Parameters.diameter = 250*1e-9;                             %Scaling parameter for the nanoparticles
-PDE_Parameters.particle_Density = 1650;                        %Density of particles (kg/m^3)
-PDE_Parameters.affinity = 2.14e-10/PDE_Parameters.dx;            %Boundary condition flux condition parameter
-PDE_Parameters.K = 150;                                          %Carrying capacity NP/Cell
-PDE_Parameters.num_Cells = 80000;
-PDE_Parameters.cell_Area = 1600e-12;
-PDE_Parameters.initial_Particle_Count = 4e8;                                %Number of particles at the beginning of the experiment
