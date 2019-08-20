2c4200d6f041bf068230b12ca16f17222cfc3078
DrStuartJohnston <47995032+DrStuartJohnston@users.noreply.github.com>
2019-08-20 14:40:51 +1000

    Fix missing file

--- Voxel-based model/Particle Properties/Parameters_282nm_Coreshell_Hela.m
+++ Voxel-based model/Particle Properties/Parameters_282nm_Coreshell_Hela.m
@@ -1,7 +1,7 @@
 PDE_Parameters.diameter = 282*1e-9;                            %Scaling parameter for the nanoparticles
 PDE_Parameters.particle_Density = 1850;                         %Density of particles (kg/m^3)
-PDE_Parameters.affinity = 1.26e-9/PDE_Parameters.dx;            %Boundary condition flux condition parameter
-PDE_Parameters.K = 220;                                          %Carrying capacity NP/Cell
+PDE_Parameters.affinity = 8.37e-10/PDE_Parameters.dx;            %Boundary condition flux condition parameter
+PDE_Parameters.K = 8;                                          %Carrying capacity NP/Cell
 PDE_Parameters.num_Cells = 100000;
-PDE_Parameters.cell_Area = 830e-12;
+PDE_Parameters.cell_Area = 1600e-12;
 PDE_Parameters.initial_Particle_Count = 1e7;                                %Number of particles at the beginning of the experiment
